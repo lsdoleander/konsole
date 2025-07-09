5a2c91f2463a8dd2df46a232ff351ce6fb56d35e
LSD.oleander <lsdoleander@gmail.com>
2025-07-09 00:12:18 -0400

    fix again

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 export StopWatch from './stopwatch'
 export gradient from 'gradient-string'
-export { debuffer, datadir } from './debugger.js'
+export { debuffer, datadir } from './debuffer.js'
 export { chalk, coloris } from './coloris.js'
 export { komponent } from './konsole.js'
 
