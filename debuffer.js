e21067f6582bb8fab484d9cf4b421a00ff35cb1c
LSD.oleander <lsdoleander@gmail.com>
2025-07-10 18:48:47 -0400

    fix exports

--- debuffer.js
+++ debuffer.js
@@ -1,4 +1,5 @@
 
+
 import fs from "node:fs"
 import os from "node:os"
 import path from 'node:path'
@@ -14,31 +15,41 @@ export function debuffer (logsdir, opts={ console:true, exceptions:true, rejecti
 		return path.join(pathname, `${l}_${zf(today.getMonth()+1) + zf(today.getDate()) + today.getFullYear()}.log`);
 	}
 
-	let def = _lf();
+    let verbose = (function isverbose() {                        
+        for (const argv of process.argv){ if (argv==="--verbose") return true; } return false;
+    })();
 
-	function _lo(path, msg) {
+	function _lo(path, msg, logger) {
 		for (let idx in msg) {
-			if (typeof msg[idx] === "object") msg[idx] = JSON.stringify(thingy, null, 2);
+			if (typeof msg[idx] === "object") {
+				if (msg[idx] instanceof Error) {
+					if (verbose || logger === "trace") {
+						msg[idx] = msg[idx].message + "\n" + msg[idx].stack;
+					} else {
+						msg[idx] = msg[idx].message
+					}
+				} else {
+					msg[idx] = JSON.stringify(msg[idx], null, 2);
+				}
+			}
 		}
 		fs.appendFileSync(path, `${msg.join(" ")}\n`, "utf-8");
 	}
 
-	let __k;
-
 	const API = {
-		console(){
-			return output;
-		},
 		logger(name){
 			let _p = _lf(name);
 			return {
 				log(...msg){
-					_lo(_p, msg);
+					_lo(_p, msg,"log");
+				},
+				debug(...msg){
+					if (verbose) _lo(_p, msg);
+				},
+				trace(...e){
+					_lo(_p, e, "trace");
 				}
 			}
-		},
-		log(...msg){
-			_lo(def, msg);
 		}
 	}
 
@@ -61,17 +72,17 @@ export function debuffer (logsdir, opts={ console:true, exceptions:true, rejecti
 
 		const intercepts = API.logger("console");
 
-        function solecons(object,method){
+        function solecons(object,method,label){
             let syscall = console[method];
             console[method] = function monkeylogger (...args) {
                 if (method === "trace") intercepts.trace("[TRACE]", ...args);
-                else intercepts.log(`[${method.toUpperCase()}`, ...args);
+                else intercepts.log(`[${(label||method).toUpperCase()}`, ...args);
             }
             return solecons;
         }
 
-        solecons(console,"log")(console,"debug")(console,"info")(console,"warn")(console,"error")(console,"trace");
-    	solecons(process.stdout,"write")(process.stderr,"write");
+        solecons(console,"log","info")(console,"debug")(console,"info")(console,"warn")(console,"error")(console,"trace");
+    	solecons(process.stdout,"write","stdout")(process.stderr,"write","stderr");
 	}
 
 	if (opts.exceptions || opts.rejections) {
