4cdc1213717eae7654df05e033e024b77f1a1456
LSD.oleander <lsdoleander@gmail.com>
2025-07-10 09:18:10 -0400

    fix exports

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 export { default as gradient } from 'gradient-string'
 export { default as chalk } from 'chalk'
 
-export { StopWatch } from './stopwatch'
+export { StopWatch } from './stopwatch.js'
 export { debuffer } from './debuffer.js'
 export { datadir } from './datadir.js'
 export { coloris } from './coloris.js'
